b60eb1cd515ef4827b482c3425666cedcaee07ba
granjerolucas <56519538+granjerolucas@users.noreply.github.com>
2024-11-18 13:24:15 -0500

    Update url.utils.js

--- src/utils/url.utils.js
+++ src/utils/url.utils.js
@@ -1,6 +1,3 @@
-export const NEXT_PUBLIC_NEWS_API_URL = (path) => {
-  return `${process.env.NEXT_PUBLIC_API_URL}${path}`;
-};
 export const baseUrl = (path) => {
-  return `${process.env.NEXT_PUBLIC_BASE_URL}/${path}`;
+  return `${process.env.NEXT_PUBLIC_BASE_URL || ""}/${path}`;
 };
