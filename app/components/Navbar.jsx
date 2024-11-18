95f111b3a0c8e418ab557b5432079936aa61c795
granjerolucas <56519538+granjerolucas@users.noreply.github.com>
2024-11-18 16:39:36 -0500

    upd: navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,38 +1,18 @@
 import { baseUrl } from "@/src/utils/url.utils";
 import Link from "next/link";
 import React from "react";
+import BaseNavbar from "./navbar/BaseNavbar";
 
 const Navbar = async () => {
-  const res = await fetch(baseUrl(`api/sources/categories`),{
+  const res = await fetch(baseUrl(`api/sources/categories`), {
     cache: "no-store",
   });
   const data = await res.json();
   return (
     <div>
       <header className="flex flex-wrap sm:justify-start sm:flex-nowrap w-full bg-white text-sm py-3 dark:bg-neutral-800 shadow-lg">
-        <nav className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-betweenx">
-          <Link
-            className="flex-none font-semibold text-xl text-black focus:outline-none focus:opacity-80 dark:text-white"
-            href="/"
-            aria-label="Brand"
-          >
-            News App
-          </Link>
-          <div className="flex flex-row items-center gap-5 mt-5 sm:justify-end sm:mt-0 sm:ps-5">
-            {data.map((item, index) => {
-              return (
-                <Link
-                  key={index}
-                  className="font-medium capitalize text-gray-600 hover:text-gray-400 focus:outline-none focus:text-gray-400 dark:text-neutral-400 dark:hover:text-neutral-500 dark:focus:text-neutral-500"
-                  href={`/news/${item.id}`}
-                  aria-current="page"
-                >
-                  {item.name}
-                </Link>
-              );
-            })}
-            
-          </div>
+        <nav className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between">
+         <BaseNavbar data={data}/>
         </nav>
       </header>
     </div>
---
+++ app/components/navbar/BaseNavbar.jsx
@@ -0,0 +1,73 @@
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+
+const BaseNavbar = ({ data }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Link
+          className="flex-none font-semibold text-xl text-black focus:outline-none focus:opacity-80 dark:text-white me-4"
+          href="/"
+          aria-label="Brand"
+        >
+          News App
+        </Link>
+        <div className="md:hidden">
+          <button
+            type="button"
+            className="hs-collapse-toggle relative size-9 flex justify-center items-center text-sm font-semibold rounded-lg border border-black/20 text-black hover:bg-white/10 focus:outline-none focus:bg-white/10 disabled:opacity-50 disabled:pointer-events-none"
+            id="hs-base-header-collapse"
+            aria-expanded="false"
+            aria-controls="hs-base-header"
+            aria-label="Toggle navigation"
+            data-hs-collapse="#hs-base-header"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+              />
+            </svg>
+
+            <span className="sr-only">Toggle navigation</span>
+          </button>
+        </div>
+      </div>
+      <div
+        id="hs-base-header"
+        className={`hs-collapse ${isOpen ? '' : 'hidden'} overflow-hidden transition-all duration-300 basis-full grow md:block`} 
+        aria-labelledby="hs-base-header-collapse"
+      >
+        <div className="overflow-hidden overflow-y-auto max-h-[75vh] [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
+          <div className="py-2 md:py-4 flex flex-col md:flex-row md:items-center md:justify-end gap-2 md:gap-5">
+            {data.map((item, index) => {
+              return (
+                <Link
+                  key={index}
+                  className="font-medium capitalize text-gray-600 hover:text-gray-400 focus:outline-none focus:text-gray-400 dark:text-neutral-400 dark:hover:text-neutral-500 dark:focus:text-neutral-500"
+                  href={`/news/${item.id}`}
+                  aria-current="page"
+                >
+                  {item.name}
+                </Link>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BaseNavbar;
